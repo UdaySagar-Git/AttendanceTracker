fe93a44abae3af99730183348a308a606134125f
Uday Sagar <udaysagar.mail@gmail.com>
2023-12-27 03:07:38 +0530

    pwa setup

--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  // disable: process.env.NODE_ENV === "development",
+  disable: false,
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
@@ -5,4 +18,4 @@ const nextConfig = {
 },
 }
 
-module.exports = nextConfig
+module.exports =  withPWA(nextConfig)
